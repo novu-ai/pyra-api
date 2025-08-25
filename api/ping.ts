26cfab01d210c03c5748439b352857336d42bf21
Alejandro Colorado <novu.future@gmail.com>
2025-08-25 13:50:17 +0800

    chore(api): make /api/ping an Edge function

--- api/ping.ts
+++ api/ping.ts
@@ -1,7 +1,9 @@
-export const runtime = 'nodejs';
+// api/ping.ts
+export const runtime = 'edge';
 
 export default async function handler() {
   return new Response(JSON.stringify({ ok: true }), {
-    headers: { 'content-type': 'application/json' }
+    headers: { 'content-type': 'application/json' },
   });
 }
+
